2ca55196035ebc02cd2afd153b8a437b4c9789fe
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-05-01 18:59:37 -0400

    a successful test with ethereal.email

--- server/utils/emailHandler.js
+++ server/utils/emailHandler.js
@@ -1,10 +1,25 @@
-import {nodemailer} from 'nodemailer'
+const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
     secure: false,
     auth: {
-        
+        user: 'helen.mckenzie@ethereal.email',
+        pass: 'MsABp8ggyck7VdHcgV'
     }
 })
+
+async function mail(){
+    const info = await transporter.sendMail({
+        from: '"Helen McKenzie" <helen.mckenzie@ethereal.email>',
+        to: 'bar@example.com, baz@example.com',
+        subject: 'hello',
+        text: 'hello world',
+        html: '<b>Hello world?</b>'
+    });
+
+    console.log("message send: %s", info.messageId);
+}
+
+mail().catch(console.error);
