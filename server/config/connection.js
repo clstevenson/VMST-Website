bd1439e5d6fa8d35fc1f0a90a119c551c8400fec
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-05-02 18:12:03 -0400

    email broken , but getting towards operational

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,3 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vmst_website');
 
 module.exports = mongoose.connection;
+
+//might add email transport connections here, we'll see
--- server/utils/emailHandler.js
+++ server/utils/emailHandler.js
@@ -2,24 +2,32 @@ const nodemailer = require("nodemailer");
 
 //fields for testing on ethereal
 const name = 'Helen McKenzie'
-const user = 'helen.mckenzie@ethereal.email'
-const password = 'MsABp8ggyck7VdHcgV'
+const user = 'imscouriertest@gmail.com'
+const password = 'p4c!jZhzNBaE2_9CYuHb!q*f'
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: {
         user: user,
-        pass: password
+        pass: password,
     }
 })
+
+transporter.verify(function (error, success){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("server ready to take msgs");
+    }
+});
 
 async function mail(){
     const info = await transporter.sendMail({
         from: `"${name}" <${user}>`,
-        to: 'bar@example.com, baz@example.com',
-        subject: 'hello',
+        to: 'ianmstevenson1@gmail.com',
+        subject: 'hello 2',
         text: 'hello world',
         html: '<b>Hello world?</b>'
     });
