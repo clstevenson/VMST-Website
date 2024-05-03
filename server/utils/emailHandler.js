851e07bf7d7f37b34e10019132c75b91045e6f6c
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-05-02 20:42:07 -0400

    email functions, not connected  front end to back end

--- server/utils/emailHandler.js
+++ server/utils/emailHandler.js
@@ -1,38 +1,56 @@
 const nodemailer = require("nodemailer");
 
-//fields for testing on ethereal
-const name = 'Helen McKenzie'
-const user = 'imscouriertest@gmail.com'
-const password = 'p4c!jZhzNBaE2_9CYuHb!q*f'
+let mailConfig, user, name;
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: user,
-        pass: password,
+if (process.env.NODE_ENV === 'production') {
+    //actual fields for sending real emails
+    mailConfig = {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD,
+        }
     }
-})
+    name = 'Ian Stevenson'
+    user = process.env.EMAIL
+} else {
+    //fields for testing on ethereal
+    mailConfig = {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'helen.mckenzie@ethereal.email',
+            pass: 'MsABp8ggyck7VdHcgV',
+        }
+    }
+    name = "test"
+    user = 'helen.mckenzie@ethereal.email'
+}
 
-transporter.verify(function (error, success){
+let transporter = nodemailer.createTransport(mailConfig);
+
+transporter.verify(function (error){
     if(error) {
-        console.log(error);
+        console.log("error setting up smtp server\n\n");
+        console.error;
+        
     } else {
         console.log("server ready to take msgs");
+        mail().catch(console.error);
     }
 });
 
 async function mail(){
     const info = await transporter.sendMail({
         from: `"${name}" <${user}>`,
-        to: 'ianmstevenson1@gmail.com',
+        to: 'test@test.test',
         subject: 'hello 2',
         text: 'hello world',
-        html: '<b>Hello world?</b>'
+        html: '<b>Hello world?</b>',
     });
 
     console.log("message send: %s", info.messageId);
 }
-
-mail().catch(console.error);
