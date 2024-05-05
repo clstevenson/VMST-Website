354febb1ac436722b408b76b085bd6fe726ebd99
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-05-05 18:22:37 -0400

    added an offcanvas selection

--- client/src/components/EmailPage/AsideSelector.jsx
+++ client/src/components/EmailPage/AsideSelector.jsx
@@ -1,20 +1,45 @@
 import { useQuery} from '@apollo/client';
+import { useState } from 'react';
 import { QUERY_LEADERS } from '../../utils/queries';
+import {Button, Offcanvas, ToggleButton} from 'react-bootstrap';
 
 export default function AsideSelector({ register, errors }) {
     const { loading, data } = useQuery(QUERY_LEADERS);
     let leaders = data?.getLeaders || [];
 
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
     return (
-        <aside>
-            <p>Select Recipients</p>
-            <ul style={{ listStyle: 'none' }}>
-                {loading ? (
-                    <li>...Loading Recipients</li>
-                ) : (leaders.map((leader) => {
-                    return <li key={Math.random()}><input type="checkbox" value={leader._id} {...register("recipient")} /> {leader.firstName} {leader.lastName}</li>
-                }))}
-            </ul>
-        </aside>
+        <>
+            <Button type='button' onClick={handleShow}>Select Recipients</Button>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Recipients</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <ul style={{ listStyle: 'none' }}>
+                        <b>Leaders</b>
+                        {loading ? (
+                            <li>...Loading Recipients</li>
+                        ) : (leaders.map((leader) => {
+                            return (
+                                <li key={Math.random()}>
+                                    <input
+                                        type="checkbox"
+                                        value={leader._id}
+                                        {...register("recipient")}
+                                    />
+                                     {leader.firstName} {leader.lastName}
+                                </li>
+                            )
+                        }))}
+                    </ul>
+
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
     )
 }
