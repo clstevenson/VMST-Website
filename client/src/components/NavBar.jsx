81e4f438fb7b55502fadde41e9dedbac1c2f04d4
Chris Stevenson <chrislstevenson@gmail.com>
2024-07-15 08:05:53 -0400

    Add styled login modal
    - net yet wired for loggin in

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,21 +3,39 @@
  Component to display a horizontal list of navigational links.
  Input prop: array of navigation objects with the following fields: id, label, href.
  The id is unique UUID, the label is the text that is displayed, and the href is the link.
+
+ Unfortunately the login modal logic needs to be contained in this component. This is
+ mostly because I am compoosing two Radix primitives both of which are tied to the same
+ nav element trigger; more generally the nav links, tooltips, and login modal are all
+ interdependent and entwined with Radix. Once it is working we can try to chop it into
+ smaller files.
  */
 
+import { useState, useEffect } from "react";
+import { useMutation } from "@apollo/client";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
-import { COLORS, QUERIES } from "../utils/constants";
-import { Image, Home, User, Info, Send } from "react-feather";
+import { Image, Home, User, Info, Send, X } from "react-feather";
 import * as Tooltip from "@radix-ui/react-tooltip";
+import * as Dialog from "@radix-ui/react-dialog";
+
 import auth from "../utils/auth";
+import { LOGIN_USER } from "../utils/mutations";
+import { COLORS, QUERIES, WEIGHTS } from "../utils/constants";
 
+// TODO Possibly use Radix navigation component/ but it must be coupled with client-side route serving
+// (Another option: add accessibility elements myself)
 export default function NavBar() {
-  // get the current page to indicate the current link
+  // tie form to state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // login checked on server
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  // TODO Need some logic here: create a filtered array depending on login status
-  // TODO Possibly use Radix navigation component but it must be coupled with client-side serving
-  // (Another option: add accessibility elements myself)
+  // will I need to useEffect to update a state variable to have NavBar re-rendederd?
+  // for example, will the NavBar automatically re-render when the authorization expires?
+  // I don't think so...
+  // const [isLoggedIn, setIsLoggedIn] = useState(auth.loggedIn());
 
   return (
     <Wrapper>
@@ -29,7 +47,12 @@ export default function NavBar() {
         {auth.loggedIn() ? (
           <NavItem href="/me" label="User" icon={User} />
         ) : (
-          <LoginItem />
+          <LoginItem
+            email={email}
+            password={password}
+            setEmail={setEmail}
+            setPassword={setPassword}
+          />
         )}
       </Tooltip.Provider>
     </Wrapper>
@@ -38,44 +61,225 @@ export default function NavBar() {
 
 // internal components for convenience
 // NavItem is for client-side routing with tooltips
+// TODO passing isCurrent as a prop causes warnings/errors; fix that
 const NavItem = ({ href, label, icon: Icon }) => {
+  // get the current page so we know which link to highlight
   const currentPage = useLocation().pathname;
   return (
     <Tooltip.Root>
       <TooltipTrigger tabIndex={-1}>
         <ListItem>
-          <NavLink to={href}>
-            <Button isCurrent={href === currentPage} tabIndex={-1}>
+          <Link to={href}>
+            <LinkButton isCurrent={href === currentPage} tabIndex={-1}>
               <Icon />
               <LabelWrapper>{label}</LabelWrapper>
-            </Button>
-          </NavLink>
+            </LinkButton>
+          </Link>
         </ListItem>
-        <TooltipContent aria-label={label}>{label}</TooltipContent>
+        <TooltipContent>{label}</TooltipContent>
       </TooltipTrigger>
     </Tooltip.Root>
   );
 };
 
-// looks like a navlink but is actually a trigger to display
-// the login modal
-const LoginItem = () => {
+// looks like a navlink but is actually a button to display the login modal
+// modal removes scrollbar which causes a shift in backdrop. Radix bug?
+const LoginItem = ({ email, password, setEmail, setPassword }) => {
+  // email validation; this is the regex the HTML form uses for validation
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    // attempt to log in
+
+    // if successful, close modal, reset states, and go to User page
+
+    // if not successful, display an error message on the modal (keeping info in input fields)
+  };
+
   return (
-    <Tooltip.Root>
-      <TooltipTrigger tabIndex={-1}>
-        <ListItem>
-          <Button onClick={() => alert("Logging in")}>
-            <User />
-            <LabelWrapper>Log In</LabelWrapper>
-          </Button>
-        </ListItem>
-      </TooltipTrigger>
-      <TooltipContent aria-label="Login">Log In</TooltipContent>
-    </Tooltip.Root>
+    <Dialog.Root>
+      <Tooltip.Root>
+        {/* Navbar item is trigger for both tooltip and login modal */}
+        <Tooltip.Trigger asChild>
+          <Dialog.Trigger asChild>
+            <ListItem>
+              <LinkButton>
+                <User />
+                <LabelWrapper>Log In</LabelWrapper>
+              </LinkButton>
+            </ListItem>
+          </Dialog.Trigger>
+        </Tooltip.Trigger>
+        <TooltipContent>Log In</TooltipContent>
+      </Tooltip.Root>
+
+      {/* Login modal window is below */}
+      <Dialog.Portal>
+        <DialogOverlay />
+          <DialogContent>
+            <Xclose asChild>
+              <X />
+            </Xclose>
+            {/* <DialogTitle>Login</DialogTitle> */}
+              <DialogDescription>
+                Enter your login information below.
+              </DialogDescription>
+              <Form onSubmit={handleSubmit}>
+                <InputWrapper>
+                  {/* TODO: add valudation that field is not empty (onBlue) */}
+                  <label htmlFor="email">Email</label>
+                  <Input
+                    type="email"
+                    id="email"
+                    required
+                    value={email}
+                    tabIndex={1}
+                    onChange={(evt) => {
+                      setEmail(evt.target.value);
+                    }}
+                  />
+                </InputWrapper>
+                <InputWrapper>
+                  {/* TODO: add valudation that field is not empty (onBlue) */}
+                  <label htmlFor="password">Password</label>
+                  <Input
+                    type="password"
+                    id="password"
+                    required
+                    value={password}
+                    tabIndex={2}
+                    onChange={(evt) => {
+                      setPassword(evt.target.value);
+                    }}
+                  />
+                </InputWrapper>
+                <DialogButtonWrapper>
+                  <DialogClose asChild>
+                    <CloseButton tabIndex={3}>Close</CloseButton>
+                  </DialogClose>
+                  <SubmitButton type="submit" tabIndex={4}>
+                    Submit
+                  </SubmitButton>
+                </DialogButtonWrapper>
+              </Form>
+          </DialogContent>
+    </Dialog.Root>
   );
 };
 
-// Styled components
+/**********************************
+ STYLED COMPONENTS
+ **********************************/
+
+// For login modal (using Radix Dialog primitive)
+// modal window styling
+const DialogContent = styled(Dialog.Content)`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  font-size: 1.1rem;
+  padding: 24px;
+  border: 1px solid ${COLORS.accent[7]};
+  border-radius: 8px;
+  background-color: white;
+  position: fixed;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: fit-content;
+  height: fit-content;
+  box-shadow: 2px 4px 8px ${COLORS.gray[9]};
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+// email label and input field
+const InputWrapper = styled.fieldset`
+  border: none;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const Input = styled.input`
+  all: unset;
+  width: 30ch;
+  padding: 2px 10px;
+  border: 1px solid ${COLORS.accent[7]};
+  border-radius: 4px;
+  outline-offset: 0;
+
+  &:focus {
+    outline: 2px solid ${COLORS.accent[10]};
+    background-color: ${COLORS.accent[3]};
+  }
+`;
+
+const CloseButton = styled.button`
+  padding: 2px 16px;
+  font-weight: ${WEIGHTS.medium};
+  background-color: ${COLORS.accent[1]};
+  border: 1px solid ${COLORS.accent[9]};
+  border-radius: 4px;
+  outline-offset: 0;
+
+  &:hover,
+  &:focus {
+    outline: 2px solid ${COLORS.accent[11]};
+    background-color: ${COLORS.accent[4]};
+  }
+`;
+
+const SubmitButton = styled(CloseButton)`
+  background-color: ${COLORS.accent[12]};
+  color: white;
+
+  &:hover,
+  &:focus {
+    background-color: ${COLORS.accent[11]};
+  }
+`;
+
+const DialogTitle = styled(Dialog.Title)`
+  font-size: 1.3em;
+  font-weight: ${WEIGHTS.medium};
+  color: ${COLORS.accent[12]};
+`;
+
+const DialogDescription = styled(Dialog.Description)``;
+
+const DialogButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  padding: 12px;
+`;
+
+const DialogOverlay = styled(Dialog.Overlay)`
+  --filter-width: 4px;
+  backdrop-filter: blur(var(--filter-width));
+  -webkit-backdrop-filter: blur(var(--filter-width));
+  background-color: ${COLORS.overlay};
+  position: absolute;
+  inset: 0;
+`;
+
+const DialogClose = styled(Dialog.Close)``;
+
+const Xclose = styled(Dialog.Close)`
+  position: absolute;
+  top: 3px;
+  right: 3px;
+`;
+
+// For the tooltips (on smaller screens) using Radix Tooltip primitive
 const TooltipTrigger = styled(Tooltip.Trigger)`
   background-color: transparent;
   border: none;
@@ -90,20 +294,7 @@ const TooltipContent = styled(Tooltip.Content)`
   }
 `;
 
-const Wrapper = styled.ul`
-  display: flex;
-  position: relative;
-  flex-direction: row;
-  font-size: 1.15rem;
-  list-style: none;
-  padding: 0 3px;
-  border-bottom: 1.5px solid ${COLORS.gray[500]};
-
-  @media ${QUERIES.tabletAndLess} {
-    font-size: 1rem;
-  }
-`;
-
+// navigation link items, including animation
 const ListItem = styled.li`
   transition: transform 400ms;
 
@@ -119,24 +310,16 @@ const ListItem = styled.li`
   }
 `;
 
-const NavLink = styled(Link)`
-  color: ${COLORS.primary};
-  text-decoration: none;
-
-  ${ListItem}:hover & {
-    color: ${COLORS.primary_light};
-  }
-`;
-
-const Button = styled.button`
+// the links wrap around the buttons, so they determine link appearance
+const LinkButton = styled.button`
   border: none;
   /* uncomment line below if you want all tabs for links (not just current page) */
-  /* border: 1px dotted ${COLORS.gray[500]}; */
+  /* border: 1px dotted ${COLORS.gray[9]}; */
   padding: 0 8px;
   padding-top: 2px;
-  color: ${COLORS.primary_dark};
+  color: ${COLORS.accent[12]};
   border: ${({ isCurrent }) => {
-    return isCurrent && `1px solid ${COLORS.gray[500]}`;
+    return isCurrent && `1px solid ${COLORS.gray[9]}`;
   }};
   border-bottom: none;
   background-color: ${({ isCurrent }) => {
@@ -147,7 +330,7 @@ const Button = styled.button`
 
   &:hover {
     cursor: pointer;
-    color: ${COLORS.primary_light};
+    color: ${COLORS.accent[9]};
   }
 
   &:focus {
@@ -155,8 +338,24 @@ const Button = styled.button`
   }
 `;
 
+// Text next to nav icons disappears on smaller screens
 const LabelWrapper = styled.span`
   @media ${QUERIES.tabletAndLess} {
     display: none;
+  }
+`;
+
+// Wraps the whole navbar
+const Wrapper = styled.ul`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  font-size: 1.15rem;
+  list-style: none;
+  padding: 0 3px;
+  border-bottom: 1.5px solid ${COLORS.gray[9]};
+
+  @media ${QUERIES.tabletAndLess} {
+    font-size: 1rem;
   }
 `;
