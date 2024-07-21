ce7c21ce957f8eeaf7b17752fe92e0c86db3b9d6
Chris Stevenson <chrislstevenson@gmail.com>
2024-07-21 10:32:11 -0400

    Modify shuffle utility to output new array
    - previously the utility would modify the input
    - this had some undesirable consequences on the behavior of the
    "Gallery" page (which now has been fixed)

--- client/src/utils/shuffle.js
+++ client/src/utils/shuffle.js
@@ -1,19 +1,22 @@
 // function to shuffle array elements
+// result is a new array; the original is not shuffled
 // taken from SO: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export default function shuffle(array) {
-  var currentIndex = array.length;
-  var randomIndex;
+  let currentIndex = array.length;
+  let randomIndex;
+  const outputArray = [...array];
 
   // While there remain elements to shuffle.
   while (currentIndex > 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+    [outputArray[currentIndex], outputArray[randomIndex]] = [
+      outputArray[randomIndex],
+      outputArray[currentIndex],
+    ];
   }
-  return array;
+  return outputArray;
 }
